2fec9eec2297fc16390bf2e2a077ee87ec00cfa0
sddd1234567 <sddd1234567@gmail.com>
2018-01-06 11:34:08 +0800

    update

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/search', async function (req, res) {
         await searchPCHomeDataFromFirebase(title).then(
             products => productList = products
         ).catch((error) => {
-            assert.isNotOk(error, 'Promise error');
+            console.log(error);
         });
         console.log(productList);
         res.render('search', { title: "啾比比價網--搜尋結果", classify: "search", productList: productList });
